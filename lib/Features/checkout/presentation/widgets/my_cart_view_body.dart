340f06ba4cadec38e654505b72530316d2d8ee2d
Ezzat Mohamed <ezat71101@gmail.com>
2023-12-09 16:02:08 +0200

    create total price widget

--- lib/Features/checkout/presentation/widgets/my_cart_view_body.dart
+++ lib/Features/checkout/presentation/widgets/my_cart_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:checkout_payment/Features/checkout/presentation/widgets/cart_info_item.dart';
+import 'package:checkout_payment/Features/checkout/presentation/widgets/total_price_widget.dart';
 import 'package:flutter/material.dart';
 
 class MyCartViewBody extends StatelessWidget {
@@ -5,14 +7,39 @@ class MyCartViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 18,
-        ),
-        Image.asset('assets/images/basket_image.png'),
-        
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 18,
+          ),
+          Image.asset('assets/images/basket_image.png'),
+          const SizedBox(
+            height: 25,
+          ),
+          const OrderInfoItem(title: 'Order Subtotal', value: r'42.97$'),
+          const OrderInfoItem(
+            title: 'Discount',
+            value: r'0$',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Shipping',
+            value: r'8$',
+          ),
+          const Divider(
+            thickness: 2,
+            height: 34,
+            color: Color(0xffC7C7C7),
+          ),
+          const TotalPrice(
+            title: 'Total', 
+            value: r'$50.97'),
+        ],
+      ),
     );
   }
 }
