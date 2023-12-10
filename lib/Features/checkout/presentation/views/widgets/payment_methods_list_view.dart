e55c622e3010cc5fd48d5417904e1afbad0339a0
Ezzat Mohamed <ezat71101@gmail.com>
2023-12-10 16:46:22 +0200

    select payment method

--- lib/Features/checkout/presentation/views/widgets/payment_methods_list_view.dart
+++ lib/Features/checkout/presentation/views/widgets/payment_methods_list_view.dart
@@ -1,12 +1,20 @@
 import 'package:checkout_payment/Features/checkout/presentation/views/widgets/payment_method_item.dart';
 import 'package:flutter/material.dart';
 
-class PaymentMethodsListView extends StatelessWidget {
+class PaymentMethodsListView extends StatefulWidget {
   const PaymentMethodsListView({super.key});
+
+  @override
+  State<PaymentMethodsListView> createState() => _PaymentMethodsListViewState();
+}
+
+class _PaymentMethodsListViewState extends State<PaymentMethodsListView> {
   final List<String> paymentMethodsItems = const [
     'assets/images/card.svg',
     'assets/images/paypal.svg',
   ];
+
+  int activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +25,18 @@ class PaymentMethodsListView extends StatelessWidget {
           itemCount: paymentMethodsItems.length,
           itemBuilder: (context, index) {
             return Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: PaymentMethodItem(
-                isActive: false,
-                image: paymentMethodsItems[index],
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: GestureDetector(
+                onTap: () {
+                  activeIndex = index;
+                  setState(() {
+                    
+                  });
+                },
+                child: PaymentMethodItem(
+                  isActive: activeIndex == index,
+                  image: paymentMethodsItems[index],
+                ),
               ),
             );
           }),
