e31b779ad1973c653625d1e7b072ff3237d13b20
Ezzat Mohamed <ezat71101@gmail.com>
2023-12-11 02:07:56 +0200

    finish thank you card

--- lib/Features/checkout/presentation/views/widgets/thank_you_card.dart
+++ lib/Features/checkout/presentation/views/widgets/thank_you_card.dart
@@ -1,7 +1,7 @@
 import 'package:checkout_payment/Features/checkout/presentation/views/widgets/payment_item_info.dart';
 import 'package:checkout_payment/Features/checkout/presentation/views/widgets/total_price_widget.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../../core/utils/styles.dart';
 import 'card_info_widget.dart';
@@ -23,28 +23,29 @@ class ThankYouCard extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.only(top: 50 + 16, left: 22, right: 22),
-        child: Column(children: [
-          const Text(
-            'Thank you!',
-            textAlign: TextAlign.center,
-            style: Styles.style25,
-          ),
-          Text(
-            'Your transaction was successful',
-            textAlign: TextAlign.center,
-            style: Styles.style20,
-          ),
-          const SizedBox(
-            height: 42,
-          ),
-          const PaymentItemInfo(
-            title: 'Date',
-            value: '01/24/2023',
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const PaymentItemInfo(
+        child: Column(
+          children: [
+            const Text(
+              'Thank you!',
+              textAlign: TextAlign.center,
+              style: Styles.style25,
+            ),
+            Text(
+              'Your transaction was successful',
+              textAlign: TextAlign.center,
+              style: Styles.style20,
+            ),
+            const SizedBox(
+              height: 42,
+            ),
+            const PaymentItemInfo(
+              title: 'Date',
+              value: '01/24/2023',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
               title: 'Time',
               value: '10:15 AM',
             ),
@@ -59,13 +60,46 @@ class ThankYouCard extends StatelessWidget {
               height: 60,
               thickness: 2,
             ),
-            const TotalPrice(title: 'Total', value: r'$50.97'), 
+            const TotalPrice(title: 'Total', value: r'$50.97'),
             const SizedBox(
               height: 30,
             ),
-            const CardInfoWidget()
-
-        ]),
+            const CardInfoWidget(),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  FontAwesomeIcons.barcode,
+                  size: 64,
+                ),
+                Container(
+                  width: 113,
+                  height: 58,
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                          width: 1.50, color: Color(0xFF34A853)),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'PAID',
+                      textAlign: TextAlign.center,
+                      style: Styles.style24.copyWith(
+                        color: const Color(0xff34A853),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: ((MediaQuery.sizeOf(context).height * .2 + 20) / 2) - 29,
+            ),
+          ],
+        ),
       ),
     );
   }
